f6cf0f84ac6af0c518a5ee56050cbf57e3c4b54b
Surbhisinghal1234 <surbhi98singhal@gmail.com>
2024-03-27 19:04:17 +0530

    resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
-
 import RegisterForm from "./RegisterForm";
-
 import Profile from "./profile/Profile";
-
 
 function App() {
   return (
     <>
-      {/* <RegisterForm /> */}
-      <Profile />
+      <RegisterForm />
+      {/* <Profile /> */}
     </>
   );
 }
